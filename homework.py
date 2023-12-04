fac88082166f1e6caee16bdba4f4c886ed861b42
Oleg Bryukhanov <bryukh2016@outlook.com>
2023-12-04 20:23:52 +0300

    first щÐщoutlines

--- homework.py
+++ homework.py
@@ -1,6 +1,27 @@
+import datetime as dt
+
+M_IN_KM: int = 1000  # константа для перевода метры в км.
+
+
 class InfoMessage:
     """Информационное сообщение о тренировке."""
-    pass
+    def __init__(self,
+                 training_type: str,
+                 duration: float,
+                 distance: float,
+                 speed: float,
+                 calories: float,
+                 ):
+        self.training_type = training_type
+        self.duration = duration
+        self.distance = distance
+        self.speed = speed
+        self.calories = calories
+
+    def get_message(self):
+        return (f'Тип тренировки: {training_type}; Длительность: {duration:.3f} ч.; '
+                f'Дистанция: {distance:.3f} км; Ср. скорость: {speed:.3f} км/ч; '
+                f'Потрачено ккал: {calories:.3f}.')
 
 
 class Training:
@@ -15,11 +36,12 @@ class Training:
 
     def get_distance(self) -> float:
         """Получить дистанцию в км."""
-        pass
+        return action * LEN_STEP / M_IN_KM
+
 
     def get_mean_speed(self) -> float:
         """Получить среднюю скорость движения."""
-        pass
+        return get_distance(self.action) / self.duration)
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
